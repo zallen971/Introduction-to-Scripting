e845e01c975c1edfa103046c9e8eedf6925320a9
Zachary Allen <zachary.allen2@snhu.edu>
2024-09-13 20:08:59 -0400

    2-3 Assignment. Get name, age, and calculate birth year then print greeting and birth year.

--- Module - 2/2-3 Assignment/NameAge.py
+++ Module - 2/2-3 Assignment/NameAge.py
@@ -1,14 +1,14 @@
 from datetime import date
 
-#get name
+# get name
 name = input("What is your name? ")
 
-#get age
+# get age
 age = int(input("What is your age? "))
 
-#calculate birth year
+# calculate birth year
 current_year = date.today().year
 birth_year = current_year - age
 
-#greetings plus birth year
+# greetings plus birth year
 print(f'Hello {name}! You were born in {birth_year}.')
